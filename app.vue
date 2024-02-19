b4e1f693c7e7bdec356be788a5d1474b83447861
aarizona <ar.ariz.arizona@gmail.com>
2024-02-19 12:23:52 +0300

    project start

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const items = [{
+  label: 'Погода на сегодня',
+  content: 'This is the content shown for Tab1'
+}, {
+  label: 'Погода на 3 дня',
+  content: 'Finally, this is the content for Tab3'
+}]
+</script>
+
+<template>
+  <UContainer>
+    <UTabs :items="items" />
+  </UContainer>
+</template>
