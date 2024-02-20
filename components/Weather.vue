6bea5a5e2ea7d4a135bc6539b68adb337709a729
aarizona <ar.ariz.arizona@gmail.com>
2024-02-20 10:57:35 +0300

    1st screen

--- components/Weather.vue
+++ components/Weather.vue
@@ -1,5 +1,15 @@
 <script setup>
+import d2d from 'degrees-to-direction';
 const { error, pending, today: data } = await $fetch('/api/today')
+
+const windDirection = d2d(data.wind.deg)
+const translating = {
+    'N': 'север',
+    'E': 'восток',
+    'S': 'юг',
+    'W': 'запад',
+}
+const windDirectionFormat = [...windDirection].map(el => translating[el]).join('-')
 </script>
 <template>
     <div v-if="pending">
@@ -8,15 +18,40 @@ const { error, pending, today: data } = await $fetch('/api/today')
     <div v-else-if="error">
         Error...
     </div>
-    <ul v-else>
-        <li>
-            <Icon name="wi:thermometer" /> {{ data.main.temp }}
-        </li>
-        <li>
-            <Icon name="wi:humidity" /> {{ data.main.humidity }}
-        </li>
-        <li>
-            <Icon name="wi:cloudy-windy" /> {{ data.wind.speed }}
-        </li>
-    </ul>
+    <a-row align="middle" v-else>
+        <a-col span="24">
+            <a-typography-title :level="3">
+                Погода на сегодня в {{ data.name }}
+            </a-typography-title>
+        </a-col>
+        <a-col span="10">
+            <a-typography>
+                <a-image :src="`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`" :width="100"
+                    :preview="false" /> сегодня {{ data.weather[0].description }}
+            </a-typography>
+        </a-col>
+        <a-col span="14">
+            <a-space direction="vertical">
+                <a-typography>
+                    <Icon name="wi:thermometer" /> Температура {{ data.main.temp }}℃, ощущается как {{
+                        data.main.feels_like }}℃
+                </a-typography>
+                <a-typography>
+                    <Icon name="wi:humidity" /> Влажность {{ data.main.humidity }}%
+                </a-typography>
+                <a-typography>
+                    <Icon name="wi:cloudy-windy" /> Ветер {{ data.wind.speed }} <sup>м</sup>/<sub>с</sub> {{
+                        windDirectionFormat }},
+                    порывы до {{
+                        data.wind.gust }} <sup>м</sup>/<sub>с</sub>
+                </a-typography>
+            </a-space>
+        </a-col>
+    </a-row>
 </template>
+
+<style>
+.icon {
+    font-size: 2.4rem;
+}
+</style>
